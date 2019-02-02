05e499f17980514cd6f16b0eac663eeb2be00677
xwy27 <xuwy27@mail2.sysu.edu.cn>
2019-02-03 01:07:03 +0800

    icon system online!

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const PATH = require('path');
+const getIconForFile = require('vscode-icons-js').getIconForFile;
 
 const template = require('../middleware/templating')();
 
@@ -11,18 +12,18 @@ let index = async (ctx, next) => {
       filename: file,
       time: stats.mtime.toUTCString().split(' GMT')[0],
       size: stats.size / 1000 + 'KB',
-      icon: ''
+      icon: getIconForFile(file)
     });
   });
 
   ctx.body = template.render('index.html', {
     title: 'W-SFS',
-    intro: 'Light, convenient, web based Static File Service',
+    intro: 'Light, Convenient, Web based Static File Service',
     files: userFiles,
   });
   await next();
 }
 
 module.exports = {
-  'GET /' : index
+  'GET /': index
 }
---
+++ files/test.js
@@ -0,0 +1,3 @@
+const getIconForFile = require('vscode-icons-js').getIconForFile;
+
+console.log(getIconForFile('readme.md'));
